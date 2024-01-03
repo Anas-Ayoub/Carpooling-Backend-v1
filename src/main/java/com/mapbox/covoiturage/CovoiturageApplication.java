09b8e2519adc3004c9e361984c8fb12dbc97a08a
yasser-sab <yasser.sabsibo@gmail.com>
2024-01-03 14:48:32 +0100

    add seeds to server

--- src/main/java/com/mapbox/covoiturage/CovoiturageApplication.java
+++ src/main/java/com/mapbox/covoiturage/CovoiturageApplication.java
@@ -1,14 +1,48 @@
 package com.mapbox.covoiturage;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import com.mapbox.covoiturage.MAPBOX.Entity.Driver;
+import com.mapbox.covoiturage.MAPBOX.Entity.Passenger;
+import com.mapbox.covoiturage.MAPBOX.Entity.Trip;
+import com.mapbox.covoiturage.MAPBOX.Entity.UserEntity;
+import com.mapbox.covoiturage.MAPBOX.Repository.DriverRepository;
+import com.mapbox.covoiturage.MAPBOX.Repository.PassengerRepository;
+import com.mapbox.covoiturage.MAPBOX.Repository.TripRepo;
+import com.mapbox.covoiturage.MAPBOX.Repository.UserRepository;
 
 @SpringBootApplication
-public class CovoiturageApplication {
+public class CovoiturageApplication implements CommandLineRunner {
+	@Autowired
+	private DriverRepository driverRep;
 
+	@Autowired
+	private PassengerRepository passengerRep;
+
+	@Autowired
+	private TripRepo tripRepo;
+
+	@Autowired
+	private UserRepository userRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(CovoiturageApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Driver d = driverRep.save(new Driver(null,"name 1","email1","tel1"));
+
+		Passenger p = passengerRep.save(new Passenger(null,"name 1","email 1","phone 1",List.of(12d)));
+
+		UserEntity u = userRepo.save(new UserEntity(null,"email","1234"));
+
+		tripRepo.save(new Trip(null, List.of(12d), List.of(12d), "car 1", new Date(), List.of(List.of(12d)), d, List.of(p)));
 	}
 
 }
